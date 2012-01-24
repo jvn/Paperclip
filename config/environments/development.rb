6bf0896c159bc36967340e21509adecedab23c04
mikkel koefoed Lindtner <mkl@mikiaholding.dk>
2012-01-24 18:14:33 +0100

    development, paperclip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Paperclip::Application.configure do
+  Paperclip.options[:command_path] = "/c/Windows/system32/"
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
