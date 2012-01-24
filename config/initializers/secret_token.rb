8012a2d8fdd6573e20d4b85cde283df9e3d3e4b2
jvn <jvn@mikiaholding.dk>
2012-01-24 17:56:39 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paperclip::Application.config.secret_token = '0bb4c2a29d9469d448678911c4370a6f814c60d11105a9cf5d708f328d3ce346ba57c176468d1afd6deb0ec87067641b55539b1c93d76c947a5dcef6f8438ed2'
